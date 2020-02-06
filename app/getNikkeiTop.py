397c262be840fa53452724a39a33fc206edafe96
bps_saito <bps_saito@bps-saitonoMacBook-puro.local>
2020-02-06 17:29:54 +0900

    スクレイピングのプロトタイプ作成

---
+++ app/getNikkeiTop.py
@@ -0,0 +1,27 @@
+# coding: UTF-8
+import urllib.request, urllib.error
+from bs4 import BeautifulSoup
+
+# アクセスするURL
+url_str = "http://www.nikkei.com/"
+
+# URLにアクセスする htmlが帰ってくる → <html><head><title>経済、株価、ビジネス、政治のニュース:日経電子版</title></head><body....
+html = urllib.request.urlopen(url=url_str)
+
+# htmlをBeautifulSoupで扱う
+soup = BeautifulSoup(html, "html.parser")
+
+# タイトル要素を取得する → <title>経済、株価、ビジネス、政治のニュース:日経電子版</title>
+title_tag = soup.title
+
+# 要素の文字列を取得する → 経済、株価、ビジネス、政治のニュース:日経電子版
+title = title_tag.string
+
+# タイトル要素を出力
+print(title_tag)
+
+# タイトルを文字列を出力
+print (title)
+
+
+
